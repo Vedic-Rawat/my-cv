ab9a070a2c81aab5f7804ab872f1aa31d7aee772
Vedic-Rawat <168581127+Vedic-Rawat@users.noreply.github.com>
2025-05-01 23:36:55 +0530

    added footer

---
+++ script.js
@@ -0,0 +1,8 @@
+var a;
+
+function alert()
+ {
+    alert("This is a custom alert function.");
+    console.log("This is a custom alert function.");    
+
+    return "This is a custom alert function.";}
